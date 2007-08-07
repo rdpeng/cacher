415b51a7d8fe1db30df96d52936237f6a1635bd0
Roger D. Peng [audrey] <rpeng@jhsph.edu>
2007-08-07 14:03:00 -0400

    Allow user to download entire cache when cloning and show progress meter

--- R/remote.R
+++ R/remote.R
@@ -1,11 +1,36 @@
 ################################################################################
 
-clonecache <- function(id, cachedir = ".cache") {
+clonecache <- function(id, cachedir = ".cache", download.cache = FALSE) {
         ## For now, 'id' is a URL like http://asdf.org/.cache
         mkdirs(cachedir)
         setConfig("cachedir", cachedir)
         initDownload(id)
         writeLines(id, file.path(cachedir, "ORIGIN"))
+
+        if(download.cache) {
+                message("downloading cache database files")
+                dbfiles <- readLines(file.path(cachedir, "DBFILES"))
+
+                for(i in seq_along(dbfiles)) {
+                        src <- file.path(dbdir(id), dbfiles[i])
+                        dest <- file.path(dbdir(cachedir), dbfiles[i])
+
+                        showMeter(i, length(dbfiles))
+                        download(src, dest)
+                }
+                cat("\nfinished\n")
+        }
+}
+
+showMeter <- function(i, n) {
+        if(n < getOption("width"))
+                cat("=", sep = "")
+        else {
+                mark <- i * getOption("width") / n
+
+                if(floor(mark) == ceiling(mark))
+                        cat("=", sep = "")
+        }
 }
 
 isClone <- function() {
@@ -53,6 +78,8 @@ initDownload <- function(id) {
                          mode = "w")
                 download(file.path(srcdir(id), srcfile),
                          file.path(srcdir(cachedir), srcfile), mode = "w")
+                download(file.path(id, "DBFILES"),
+                         file.path(cachedir, "DBFILES"), mode = "w")
         }
 }
 
