d6623e81ab6dc4880247f5f889f48f9dc4985d12
Roger D. Peng [audrey] <rpeng@jhsph.edu>
2007-09-07 15:08:24 -0400

    typo in archive URL

--- R/zzz.R
+++ R/zzz.R
@@ -1,6 +1,6 @@
 .onLoad <- function(lib, pkg) {
         setConfig("exprDeparseWidth", 30)
         setConfig("cachedir", ".cache")
-        setConfig("archive", "http:///www.biostat.jhsph.edu/rr")
+        setConfig("archive", "http://www.biostat.jhsph.edu/rr")
 }
 
