ea2c37204571fff10b9a4154bc6e24475be28ce6
Roger D. Peng [audrey] <rpeng@jhsph.edu>
2007-10-30 11:10:24 -0400

    Add the 'skip' argument to 'sha1'
    This argument is useful if we want to compare results to the
    'digest' package which by default uses 'skip=14' for R objects.

--- R/hash.R
+++ R/hash.R
@@ -1,4 +1,6 @@
-sha1 <- function(object) {
-	r <- serialize(object, NULL)
-	.Call("hash_object", r)
+sha1 <- function(object, skip = 0L) {
+	## Setting 'skip = 14' gives us the same results as
+	## 'digest(object, "sha1")'	
+	bytes <- serialize(object, NULL)
+	.Call("sha1_object", bytes, as.integer(skip))
 }
--- src/hash.c
+++ src/hash.c
@@ -3,18 +3,32 @@
 #include <Rdefines.h>
 #include "sha1.h"
 
-SEXP hash_object(SEXP object)
+/* 
+ * This code is adapted from the 'digest.c' code in the 'digest'
+ * package
+ */
+
+SEXP sha1_object(SEXP object, SEXP skip_bytes)
 {
 	char output[41];  /* SHA-1 is 40 bytes + '\0' */
-	char *data;
-	int nChar, i;
+	Rbyte *data;
+	int nChar, i, skip;
 	SEXP result;
 	sha1_context ctx;
 	unsigned char buffer[20];
 
-	data = (char *) RAW(object);
-	nChar = LENGTH(object);
+	data = RAW(object);
+	nChar = length(object);
+	skip = INTEGER(skip_bytes)[0];
 	
+	if(skip > 0) {
+		if(skip >= nChar)
+			nChar = 0;
+		else {
+			nChar -= skip;
+			data += skip;
+		}
+	}
 	sha1_starts(&ctx);
 	sha1_update(&ctx, (uint8 *) data, nChar);
 	sha1_finish(&ctx, buffer);
